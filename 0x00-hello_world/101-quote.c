f5ea96814e16c720e4770bfe859e094548ac73be
OSDevBe <oussama.sa@gmail.com>
2023-10-05 15:45:44 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a quote 
+ * 			without using printf or puts to stderr
+ *
+ * Return: return 1 (Error)
+*/
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	write(1, quote,59);
+
+	return (1);
+}
