0701ca06d78ee84aad4c3d586f845a7a4002819f
OSDevBe <oussama.sa@gmail.com>
2023-10-05 15:51:06 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,14 @@
  * main - Entry point
  *
  * Description: A C program that prints a quote
- * 			without using printf or puts to stderr
+ *		without using printf or puts to stderr
  *
  * Return: return 1 (Error)
 */
 
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char com[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quote, 59);
 
