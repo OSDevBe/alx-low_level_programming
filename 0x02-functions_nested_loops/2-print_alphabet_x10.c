ff729a28541ac7806b132a6738d50c2944706eac
OSDevBe <oussama.sa@gmail.com>
2023-10-11 00:35:51 +0100

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet a-z 10 times
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int i = 0, alpha;
+
+	while (i <= 9)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchar('\n');
+		i++;
+	}
+}
