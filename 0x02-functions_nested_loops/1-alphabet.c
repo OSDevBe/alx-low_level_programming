c745de39fe622f8c56a699224c66d09a031b7655
OSDevBe <oussama.sa@gmail.com>
2023-10-11 00:29:13 +0100

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+
+/**
+ * print_alphabet - print alphabet from a to z
+*/
+
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
