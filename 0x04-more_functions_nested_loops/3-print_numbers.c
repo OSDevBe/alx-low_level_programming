84bc5d1b5cf51a8fc0615120e48944cfbcee9170
OSDevBe <oussama.sa@gmail.com>
2023-10-14 00:16:33 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0 - 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n= 48;
+
+	while(n <= 57)
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
