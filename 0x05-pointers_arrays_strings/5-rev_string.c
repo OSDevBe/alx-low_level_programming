b0a407a12493fe35bfbceaafc1ebab560a1769f8
OSDevBe <oussama.sa@gmail.com>
2023-10-17 02:56:23 +0100

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ *
+ * @s: arg ptr to str
+ *
+ * Return: NULL
+ */
+
+void rev_string(char *s)
+{
+	int i, j, len = 0;
+	char tmp;
+
+	while (s[len] != '\0')
+		len++;
+
+	i = len - 1;
+	j = 0;
+
+	while (i >= 0)
+	{
+		tmp = s[j];
+		s[j] = s[i];
+		s[i] = tmp;
+		i--;
+		j++;
+	}
+}
