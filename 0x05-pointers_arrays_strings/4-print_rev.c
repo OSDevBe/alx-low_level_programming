3ff4b9f404125ffc7dc3a3bb1302ff1ac191b0ce
OSDevBe <oussama.sa@gmail.com>
2023-10-17 01:55:48 +0100

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - print the string in rev
+ *
+ * @s: arg pointer to string
+ *
+ * Return: NULL
+ */
+
+void print_rev(char *s)
+{
+	int i, len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	i = len;
+
+	while (i >= 0)
+		_putchar(s[i--]);
+
+	_putchar('\n');
+}
