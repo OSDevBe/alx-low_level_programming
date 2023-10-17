2a44c8c357095a6381f8d2fc75bdc185ed8838d8
OSDevBe <oussama.sa@gmail.com>
2023-10-17 01:38:51 +0100

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - manual strlen
+ *
+ * @s: arg string ptr
+ *
+ * Return: s length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	while(*s++ != '\0')
+		i++;
+
+	return (i;
+}
