073ee82e05878354522630d2879f54444677a25b
OSDevBe <oussama.sa@gmail.com>
2023-10-20 01:51:22 +0100

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string n char
+ *
+ * @src: arg ptr string source
+ * @dest: arg pts string destination
+ * @n: arg int nmbr of char
+ *
+ * Return: ptr string of dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+		dest[i] = src[i++];
+
+	while (i < n)
+		dest[i++] = '\0';
+
+	return (dest);
+}
