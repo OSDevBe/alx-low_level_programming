58f4607b2cdfa956b0693837732a7d1a4205a897
OSDevBe <oussama.sa@gmail.com>
2023-10-21 04:19:39 +0100

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string - cap first char of a word
+ *
+ * @str: arg ptr to string
+ *
+ * Return: arg ptr of string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0, j;
+	char scar[13] = {' ', '\t', '\n', ',', ';', '.',
+						'!', '?', '"', '(', ')', '{', '}'};
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (i == 0)
+			{
+				str[i] = str[i] - 32;
+			}
+			else
+			{
+				for (j = 0; j < 13; j++)
+				{
+					if (str[i - 1] == scar[j])
+						str[i] = str[i] - 32;
+				}
+			}
+			
+		}
+		i++;
+	}
+	return (str);
+}
