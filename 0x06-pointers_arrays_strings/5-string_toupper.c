de36884d73d070b8492379e427ae08ec985db211
OSDevBe <oussama.sa@gmail.com>
2023-10-21 03:55:50 +0100

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - from lower to upper
+ *
+ * @s: arg ptr to string
+ *
+ * Return: arg ptr of string
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'A' && s[i] <= "Z")
+			s[i] = s[i] - ('a' - 'A');
+		i++;
+	}
+	return (s);
+}
