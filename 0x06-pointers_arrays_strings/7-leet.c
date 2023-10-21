7133405afd07d797277d76d7b6fd2a6564e8e237
OSDevBe <oussama.sa@gmail.com>
2023-10-21 04:40:16 +0100

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,2 +1,29 @@
 #include "main.h"
 
+/**
+ * leet - mozart 1337
+ * =
+ * @str: arg ptr to string
+ *
+ * Return: arg ptr of string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char lchar[] = "aAeEoOtTlL";
+	char lrep[] = "4433007711";
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (lchar[j] == str[i])
+				str[i] = lrep[j];
+		}
+		i++;
+	}
+	return (str);
+}
