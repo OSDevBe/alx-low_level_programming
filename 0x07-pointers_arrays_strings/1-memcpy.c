be3c6562c2fdde79ef5f1a88cb65ca11f7a62906
OSDevBe <oussama.sa@gmail.com>
2023-10-24 21:51:56 +0100

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies @n bytes from @src to @dest
+ *
+ * @n: arg u int 
+ *
+ * @src: arg char * src
+ *
+ * @dest: arg char * dest
+ *
+ * Return: ptr to @dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
