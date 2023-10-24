1f02539dd6bd593f8d923b5e9b7c6c7bd1a9ba8c
OSDevBe <oussama.sa@gmail.com>
2023-10-24 21:46:02 +0100

    task 0

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x07. C - more more Pointers, arrays and strings
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include<stdio.h>
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* _MAIN_H_ */
