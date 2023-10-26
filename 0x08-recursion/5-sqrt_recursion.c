70569bdbde830eaa612777d811bdab103aa1ce52
OSDevBe <oussama.sa@gmail.com>
2023-10-26 02:46:03 +0100

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * check - functtion for the square root
+ *
+ * @tmp: int
+ *
+ * @n: int
+ *
+ * Return: sqrt int
+ */
+
+int _fsqrt(int tmp, int n)
+{
+	if (tmp * tmp == n)
+		return (tmp);
+	if (tmp * tmp > n)
+		return (-1);
+	return (_fsqrt(tmp + 1, n));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root
+ *
+ * @n: int
+ *
+ * Return: square root or -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (-1);
+	return (_fsqrt(1, n));
+}
