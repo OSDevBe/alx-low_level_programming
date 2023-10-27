59f36b06b2b692bf4f6f13caceeb8b4f13dd25e2
OSDevBe <oussama.sa@gmail.com>
2023-10-28 00:26:05 +0100

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print nbr of args
+ *
+ * @argc: arg int
+ *
+ * @argv: arg ptr ptr string
+ *
+ * Return: 0 (SUCCESS)
+ */
+
+int main(int argc, char __attribute__((unused)) **argv)
+{
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
