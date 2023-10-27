c5ab0f486a6433b42c05b00889deeb544e62e056
OSDevBe <oussama.sa@gmail.com>
2023-10-28 00:22:10 +0100

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print program name
+ *
+ * @argc: arg int
+ *
+ * @argv: arg ptr ptr string
+ *
+ * Return: 0 (SUCCESS)
+ */
+
+int main(int __attribute__((unused)) argc, char **argv)
+{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
