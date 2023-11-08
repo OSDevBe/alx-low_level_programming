4c9810e6aa403bb693746e3ea9f428db2e1f9b03
OSDevBe <oussama.sa@gmail.com>
2023-11-08 23:33:06 +0100

    task 0.3

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,9 @@
 #ifndef _FUNCTION_POINTER_H_
 #define _FUNCTION_POINTER_H_
 
-#include <stddef.h>
+#include <stdio.h>
 
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 
 #endif /* _FUNCTION_POINTER_H_ */
